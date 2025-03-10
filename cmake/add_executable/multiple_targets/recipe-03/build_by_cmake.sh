a9964c3a10b1c89bd7a9045740bf07cdfbc28f3f
xu.he <xu.he@phigent.ai>
2025-03-10 17:57:06 +0800

    update cmake/add_executable/multiple_targets

--- cmake/add_executable/multiple_targets/recipe-03/build_by_cmake.sh
+++ cmake/add_executable/multiple_targets/recipe-03/build_by_cmake.sh
@@ -4,6 +4,6 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir} -D fmt_DIR=${HOME}/local/fmtlib/lib/cmake/fmt
+cmake -S ${src_dir} -B ${build_dir}
 cmake --build ${build_dir} -j 4
 
