f7737290beeee96f9a79651392fa1b8440e8e70e
hexu <hexu_1985@sina.com>
2025-05-31 20:53:11 +0800

    update cmake

--- cmake/add_executable/single_target/recipe-02/build_by_cmake.sh
+++ cmake/add_executable/single_target/recipe-02/build_by_cmake.sh
@@ -4,6 +4,6 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir} -D GTEST_ROOT=${HOME}/local/googletest
+cmake -S ${src_dir} -B ${build_dir} -D GTest_ROOT=${HOME}/local/googletest
 cmake --build ${build_dir} -j 4
 
